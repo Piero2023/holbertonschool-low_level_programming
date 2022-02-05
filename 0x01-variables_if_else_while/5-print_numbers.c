20af551ca16b900f37378479c71b81f2f0dbd6d0
piero2023 <estibensmax2022@gmail.com>
2022-02-04 17:17:59 -0800

    Print all single-digit numbers in base 10 starting at 0, followed by a newline

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
