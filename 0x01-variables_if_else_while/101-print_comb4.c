3986f0bf273474888cf3a4d363ba54505fc977b2
piero2023 <estibensmax2022@gmail.com>
2022-02-04 20:40:59 -0800

    Print all the different possible combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int  a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+
+	for (b = a + 1; b <= 56; b++)
+	{
+
+	for (c = b + 1; c <= 57; c++)
+	{
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	if (a == 55 && b == 56 && c == 57)
+	{
+	}
+	else
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	}
+	putchar(10);
+
+	return (0);
+}
