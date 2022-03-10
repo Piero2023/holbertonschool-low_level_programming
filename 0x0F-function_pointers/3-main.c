f40b410921dc912bf4be20c3163e58204fb49ced
piero2023 <estibensmax2022@gmail.com>
2022-03-09 19:01:32 -0800

    A program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,10 +1,11 @@
 #include "3-calc.h"
 
 /**
- * main - searchs for an integer
- * @argc: number of arguments
- * @argv: array with argument
- * Return: no return
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: return
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
@@ -13,32 +14,21 @@ int main(int argc __attribute__((unused)), char *argv[])
 	int res, var1, var2;
 	int (*pf)(int, int);
 
-	if (argc > 4)
-	{
-	printf("Error\n");
-	exit(98);
-	}
-	if (argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-'
-	&& argv[2][0] != '%' && argv[2][0] != '/')
-	{
-	printf("Error\n");
-	exit(99);
-	}
-
+	if (argc != 4)
+	{	printf("Error\n");
+		exit(98); }
+	if ((argv[2][0] != '-' && argv[2][0] != '%' && argv[2][0] != '/'
+			&& argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-')
+			|| argv[2] == NULL || argv[2][1] != '\0')
+	{	printf("Error\n");
+		exit(99);	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
-	{
-	printf("Error\n");
-	exit(100);
-	}
-
+	{	printf("Error\n");
+		exit(100);	}
 	var1 = atoi(argv[1]);
 	var2 = atoi(argv[3]);
-
-pf = get_op_func(argv[2]);
-
+	pf = get_op_func(argv[2]);
 	res = pf(var1, var2);
 	printf("%d\n", res);
-
-
 	return (0);
 }
