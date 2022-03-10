e142df8b0a3da53a105e322e3eef2c95ae0afce6
piero2023 <estibensmax2022@gmail.com>
2022-03-10 13:58:22 -0800

    exercise 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,6 +20,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		cadena = va_arg(parametros, char *);
+		if (cadena == NULL)
+			cadena = "(nil)";
 
 		if (separator == NULL)
 			printf("%s", cadena);
