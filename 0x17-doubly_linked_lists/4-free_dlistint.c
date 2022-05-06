90842c1ab9f52db67621669588b2c1fc5a24b928
piero2023 <estibensmax2022@gmail.com>
2022-05-05 17:32:50 -0700

    Write a function that frees a dlistint_t list.

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Double pointer to struct.
+ *
+ * Return: Nothing.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *new;
+
+	while (head != NULL)
+	{
+		new = head->next;
+		free(head);
+		head = new;
+	}
+}
